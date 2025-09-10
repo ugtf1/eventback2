59605366528dde86b1d2c538820f23a322ee30db
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-09-10 15:25:51 +0100

    cloud deploy

--- halls/models.py
+++ halls/models.py
@@ -4,7 +4,7 @@ class Hall(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="hall_images/", blank=True, null=True)
+    image_url = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
